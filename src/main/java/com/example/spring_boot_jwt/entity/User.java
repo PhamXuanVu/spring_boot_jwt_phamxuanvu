ac156a37e2b01baed33c1f9cab0bb551606c8049
PhamXuanVu <xvu0425@gmail.com>
2021-09-23 13:59:04 +0700

    add register

--- src/main/java/com/example/spring_boot_jwt/entity/User.java
+++ src/main/java/com/example/spring_boot_jwt/entity/User.java
@@ -20,5 +20,6 @@ public class User extends BaseEntity {
         @OneToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
         @JoinTable(name = "t_user_role", joinColumns = {@JoinColumn(name = "user_id")},
                 inverseJoinColumns = {@JoinColumn(name = "role_id")})
+
         private Set<Role> roles = new HashSet<>();
 }
